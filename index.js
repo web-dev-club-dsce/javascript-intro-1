4e217b4880417f2793fa6a2083ef012be294dd38
tharunShiv <tharun.rocky14@gmail.com>
2019-02-28 19:13:21 +0530

    first push wdc

---
+++ index.js
@@ -0,0 +1,37 @@
+
+// // foo = 100
+// // console.log("Variable Foo = ", foo)
+
+
+// // // declaration of var
+// // var foo;
+
+// // let bar = 12;
+// // console.log("Variable Bar = ", bar)
+
+// // let bar;
+
+// // Hoisting
+
+// // For Loops
+// // for (var i = 0; i < 5; i++) {
+// //     console.log(i);
+// // }
+
+// // console.log(i)
+// // // console.log(i);
+
+// // for (let i = 0; i < 5; i++) {
+// //     console.log(i);
+// // }
+
+// // console.log(i)
+
+// // alert("You have entered the value")
+
+// var response = prompt("Do you want to continue")
+// console.log(response)
+
+// var myArray = [1, 2, 3, 4]
+
+// console.log(myArray)
